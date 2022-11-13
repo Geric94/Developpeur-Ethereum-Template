50410b77a66bb8823ed361e8129e5975cf2a8fb1
Geric94 <eric.gausi@free.fr>
2022-11-13 18:03:28 +0100

    index.js

--- 6. Dapp/client/src/index.js
+++ 6. Dapp/client/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  //EGA à commenter si cela gène console.log
   <React.StrictMode>
     <App />
   </React.StrictMode>
